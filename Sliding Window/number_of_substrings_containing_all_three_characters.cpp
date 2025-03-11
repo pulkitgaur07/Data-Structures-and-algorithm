4238a11d0856f55c47555837693c01afc4098c68
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-03-11 22:19:37 +0530

    Number Of Substrings Containing All Three Characters

---
+++ Sliding Window/number_of_substrings_containing_all_three_characters.cpp
@@ -0,0 +1,72 @@
+// Question : Given a string s consisting only of characters a, b and c.
+
+// Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+// Example 1:
+
+// Input: s = "abcabc"
+// Output: 10
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+// Example 2:
+
+// Input: s = "aaacb"
+// Output: 3
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+// Example 3:
+
+// Input: s = "abc"
+// Output: 1
+
+// Constraints:
+
+// 3 <= s.length <= 5 x 10^4
+// s only consists of a, b or c characters.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            int a=0, b=0, c=0;
+            for(int j=i;j<n;j++){
+                if(s[j] == 'a') a++;
+                else if(s[j] == 'b') b++;
+                else c++;
+                if(a && b && c){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int i=0, j=0, a=0, b=0, c=0;
+        int ans = 0;
+        while(j<n){
+            if(s[j] == 'a') a++;
+            else if(s[j] == 'b') b++;
+            else c++;
+            if(a && b && c){
+                while(a && b && c && i<=j){
+                    ans += (n-j);
+                    if(s[i] == 'a') a--;
+                    else if(s[i] == 'b') b--;
+                    else c--;
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+};
