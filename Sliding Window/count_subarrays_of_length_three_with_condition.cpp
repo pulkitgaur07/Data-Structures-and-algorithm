2103db34aba40bf3cf168d7712c60c6760345b32
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-27 21:20:44 +0530

    Count Subarrays of Length Three with a Condition

---
+++ Sliding Window/count_subarrays_of_length_three_with_condition.cpp
@@ -0,0 +1,41 @@
+// Question : Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
+
+// Example 1:
+
+// Input: nums = [1,2,1,4,1]
+// Output: 1
+// Explanation:
+// Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
+
+// Example 2:
+// Input: nums = [1,1,1]
+// Output: 0
+// Explanation:
+// [1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
+
+// Constraints:
+
+// 3 <= nums.length <= 100
+// -100 <= nums[i] <= 100
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 2) return 0;
+        int ans = 0;
+        for(int i=2;i<n;i++){
+            int a = nums[i-2];
+            int b = nums[i];
+            int c = nums[i-1];
+            if( (a+b) == c/2.0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
