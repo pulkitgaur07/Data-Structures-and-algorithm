9b6b5b4333f138588b4113847be731af3ddb35bf
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-06-12 07:38:48 +0530

    K closest elements

---
+++ Heap/k_closest_elements.cpp
@@ -0,0 +1,64 @@
+// Question : You are given a sorted array arr[] of unique integers, an integer k, and a target value x. Return exactly k elements from the array closest to x, excluding x if it exists.
+
+// An element a is closer to x than b if:
+
+// |a - x| < |b - x|, or
+
+// |a - x| == |b - x| and a > b (i.e., prefer the larger element if tied)
+
+
+// Return the k closest elements in order of closeness.
+
+// Examples:
+
+// Input: arr[] = [1, 3, 4, 10, 12], k = 2, x = 4
+// Output: 3 1
+// Explanation: 4 is excluded, Closest elements to 4 are: 3 (1), 1 (3). So, the 2 closest elements are: 3 1
+// Input: arr[] = [12, 16, 22, 30, 35, 39, 42, 45, 48, 50, 53, 55, 56], k = 4, x = 35
+// Output: 39 30 42 45
+// Explanation: First closest element to 35 is 39.
+// Second closest element to 35 is 30.
+// Third closest element to 35 is 42.
+// And fourth closest element to 35 is 45.
+// Constraints:
+// 1 ≤ arr.size() ≤ 105
+// 1 ≤ k ≤ arr.size()
+// 1 ≤ x ≤ 106
+// 1 ≤ arr[i] ≤ 106
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        int n = arr.size();
+        priority_queue<pair<int,int>>pq;
+        for(auto it:arr){
+            if(it==x){
+                continue;
+            }
+            else if(pq.size()<k){
+                pq.push({abs(x-it),it});
+            }
+            else if(pq.top().first>abs(x-it) 
+            || pq.top().first==abs(x-it) && pq.top().second<it){
+                pq.pop();
+                pq.push({abs(x-it),it});
+            }
+        }
+        vector<int>ans(pq.size());
+        while(pq.size()){
+            int val = pq.top().second;
+            pq.pop();
+            ans[pq.size()] = val;
+        }
+        for(int i=1;i<ans.size();i++){
+            if(abs(x-ans[i]) == abs(x-ans[i-1])){
+                swap(ans[i],ans[i-1]);
+            }
+        }
+        return ans;
+    }
+};
