21db3b7696d19d178c87b55ed981f8b279fd2c88
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-07-18 22:24:34 +0530

    LCM Triplet

---
+++ Array/lcm_triplet.cpp
@@ -0,0 +1,29 @@
+// Question : Given a number n, find the maximum possible LCM that can be obtained by selecting three numbers less than or equal to n.
+// Note : LCM stands for Lowest Common Multiple.
+
+// Examples:
+
+// Input: n = 9
+// Output: 504
+// Explanation: 504 is the maximum LCM that can be attained by any triplet of numbers less than or equal 9. The triplet which has this LCM is {7, 8, 9}.
+// Input: n = 7
+// Output: 210
+// Explanation: 210 is the maximum LCM that can be attained by any triplet of numbers less than or equal 7. The triplet which has this LCM is {5, 6, 7}.
+// Constraints:
+// 1 ≤ n ≤ 103
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int lcmTriplets(int n) {
+        if(n<=2) return n;
+        if(n%2==0 && n%3==0)
+            return (n-2)*(n-1)*(n-3);
+        else if(n%2==0)
+            return n*(n-1)*(n-3);
+        return n*(n-1)*(n-2);
+    }
+};
